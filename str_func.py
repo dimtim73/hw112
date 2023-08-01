34e898dbbdd1b55ccfe734745065eafa74d2fba2
Dmitry Timirov <timirovda@gmail.com>
2023-08-01 21:48:25 +0500

    make def lower_to_capitalize

--- str_func.py
+++ str_func.py
@@ -1,3 +1,11 @@
 def lower_to_upper():
     text = input()
     return text.upper()
+
+
+"""
+made new function lower_to_capitalize
+"""
+def lower_to_capitalize():
+    text = input()
+    return text.capitalize()
